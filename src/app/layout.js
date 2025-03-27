7740f401fbfdee5f3f984703c3fc14b234fe7a09
Kory <koriellopez03@gmail.com>
2025-03-27 13:14:16 -0500

    added & positioned header

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,14 +9,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`flex`}>
-        <NavBar
-          navs={[
-            { title: "About Us", nav: "/about-us" },
-            { title: "Events", nav: "/events" },
-          ]}
-        />
-        {children}
+      <body className="relative">
+        <div className="fixed w-full bg-zinc-500 h-32"></div>
+        <div className="flex">
+          <NavBar
+            navs={[
+              { title: "About Us", nav: "/about-us" },
+              { title: "Events", nav: "/events" },
+            ]}
+          />
+          <div className="ms-40 mt-32 p-3">{children}</div>
+        </div>
       </body>
     </html>
   );
