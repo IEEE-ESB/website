b835a72a268e285579b65a8f241a8762e491d2d7
Kory <koriellopez03@gmail.com>
2025-04-07 11:23:07 -0500

    banner changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="relative">
-        <div className="fixed w-full bg-zinc-500 h-32 flex justify-between px-5 text-yellow-300 text-6xl font-bold items-center">
-          <div>IEEE</div>
-          <div>Edinburg Student Branch</div>
+        <div className="fixed w-full h-32 flex justify-between px-5 bg-[url(/AI-PCB-1.png)] bg-cover bg-center">
+          <div className="flex h-ful bg-black/70 items-center text-white text-6xl font-extrabold">
+            <img src="/IEEEicon.png" className="h-28" />
+            <div>IEEE</div>
+          </div>
+          <div className="h-full flex items-center bg-black/70 text-white text-6xl font-bold">
+            <div>Edinburg Student Branch</div>
+          </div>
         </div>
         <div className="flex">
           <NavBar
