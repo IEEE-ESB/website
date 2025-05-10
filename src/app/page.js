7bb895a3fb1fb65fe34a225e85982e82e0d24a0f
Kory <koriellopez03@gmail.com>
2025-05-10 01:02:53 -0500

    part of summary

--- src/app/page.js
+++ src/app/page.js
@@ -10,116 +10,12 @@ export default function Home() {
       <p className="text-5xl font-bold text-primary_dark">About Us</p>
       <div className="flex flex-col gap-3 w-3/4">
         <p>
-          Voluptate officia ullamco proident est irure eu anim ipsum duis
-          consequat duis. Fugiat ut qui mollit ea aute dolore ut duis.
-          Consectetur esse elit est dolor sit cillum in incididunt sit. Ea
-          adipisicing nostrud culpa laborum pariatur eu adipisicing veniam
-          incididunt. Aliqua velit pariatur et deserunt aliquip. Do culpa anim
-          enim est proident eu officia reprehenderit mollit laborum consectetur
-          culpa qui. Minim deserunt velit id ad fugiat mollit aliquip esse. Eu
-          officia aliqua ipsum occaecat ullamco sunt. Est eu non ut in fugiat
-          qui qui commodo consectetur nulla irure do tempor. Elit magna elit
-          irure minim aliqua voluptate velit Lorem ipsum mollit.
-        </p>
-        <p>
-          Aliquip occaecat minim est veniam minim esse amet ad amet aute cillum
-          excepteur velit veniam. Nulla aute ex commodo ea esse laborum do
-          pariatur. Commodo velit minim ad id veniam excepteur. Dolore esse enim
-          occaecat ipsum.Duis ut Lorem eiusmod nulla irure consequat occaecat
-          cillum amet labore est consectetur. Consequat officia sint proident
-          officia proident dolor. Eiusmod proident fugiat consequat fugiat
-          mollit nisi dolore ipsum esse reprehenderit. Sit exercitation esse
-          reprehenderit veniam dolore dolor cillum sint voluptate aliquip. Sunt
-          dolor est qui adipisicing duis qui velit eu officia et velit. Veniam
-          voluptate in nostrud enim magna cupidatat.
-        </p>
-        <p>
-          Ea pariatur Lorem elit sunt adipisicing officia voluptate officia
-          dolore ex proident tempor velit. Minim veniam non culpa nisi duis
-          occaecat occaecat eiusmod amet ipsum. Ex laborum aliquip ut officia
-          nisi ut sit id cillum pariatur. Minim est quis cupidatat mollit
-          eiusmod do anim dolore amet quis consectetur. Exercitation officia
-          nulla deserunt voluptate.Lorem voluptate proident ad enim Lorem sunt
-          id occaecat. Elit ex eu do aliquip enim enim non laboris. Voluptate
-          incididunt tempor cupidatat duis et et eu est sunt.
-        </p>
-        <p>
-          Deserunt deserunt consequat ut eu sit irure voluptate qui. Cupidatat
-          excepteur velit incididunt mollit dolore proident laborum aliqua
-          proident eiusmod culpa cillum id. Pariatur ea mollit occaecat deserunt
-          sit aliqua excepteur do do eiusmod nostrud cupidatat laborum.
-          Cupidatat laboris ex aute minim sint. Nulla esse aliquip Lorem elit
-          Lorem fugiat et laborum laborum.Aliqua aute nostrud cupidatat laborum
-          veniam dolore voluptate Lorem excepteur ut ut qui eu mollit. Elit
-          officia dolore labore est sunt tempor aute quis in fugiat adipisicing
-          Lorem sint nulla. Dolore reprehenderit mollit est dolor. Nisi mollit
-          non consectetur ullamco consequat amet magna commodo voluptate.
-        </p>
-        <p>
-          Consectetur eu excepteur ipsum aute. Dolore sint dolor occaecat quis
-          fugiat elit velit eu adipisicing. Ullamco adipisicing sit anim minim
-          dolor. Sint deserunt quis elit adipisicing non ex. Consectetur eiusmod
-          duis et culpa aliqua tempor ea magna consectetur.Consequat in ad sint
-          incididunt nostrud amet eiusmod non laboris occaecat et veniam dolor
-          enim. Mollit exercitation eiusmod ad dolore. Sit culpa consequat irure
-          laborum reprehenderit excepteur qui aute magna commodo ad eu. Sint qui
-          officia est sit nulla.
-        </p>
-        <p>
-          Voluptate officia ullamco proident est irure eu anim ipsum duis
-          consequat duis. Fugiat ut qui mollit ea aute dolore ut duis.
-          Consectetur esse elit est dolor sit cillum in incididunt sit. Ea
-          adipisicing nostrud culpa laborum pariatur eu adipisicing veniam
-          incididunt. Aliqua velit pariatur et deserunt aliquip. Do culpa anim
-          enim est proident eu officia reprehenderit mollit laborum consectetur
-          culpa qui. Minim deserunt velit id ad fugiat mollit aliquip esse. Eu
-          officia aliqua ipsum occaecat ullamco sunt. Est eu non ut in fugiat
-          qui qui commodo consectetur nulla irure do tempor. Elit magna elit
-          irure minim aliqua voluptate velit Lorem ipsum mollit.
-        </p>
-        <p>
-          Aliquip occaecat minim est veniam minim esse amet ad amet aute cillum
-          excepteur velit veniam. Nulla aute ex commodo ea esse laborum do
-          pariatur. Commodo velit minim ad id veniam excepteur. Dolore esse enim
-          occaecat ipsum.Duis ut Lorem eiusmod nulla irure consequat occaecat
-          cillum amet labore est consectetur. Consequat officia sint proident
-          officia proident dolor. Eiusmod proident fugiat consequat fugiat
-          mollit nisi dolore ipsum esse reprehenderit. Sit exercitation esse
-          reprehenderit veniam dolore dolor cillum sint voluptate aliquip. Sunt
-          dolor est qui adipisicing duis qui velit eu officia et velit. Veniam
-          voluptate in nostrud enim magna cupidatat.
-        </p>
-        <p>
-          Ea pariatur Lorem elit sunt adipisicing officia voluptate officia
-          dolore ex proident tempor velit. Minim veniam non culpa nisi duis
-          occaecat occaecat eiusmod amet ipsum. Ex laborum aliquip ut officia
-          nisi ut sit id cillum pariatur. Minim est quis cupidatat mollit
-          eiusmod do anim dolore amet quis consectetur. Exercitation officia
-          nulla deserunt voluptate.Lorem voluptate proident ad enim Lorem sunt
-          id occaecat. Elit ex eu do aliquip enim enim non laboris. Voluptate
-          incididunt tempor cupidatat duis et et eu est sunt.
-        </p>
-        <p>
-          Deserunt deserunt consequat ut eu sit irure voluptate qui. Cupidatat
-          excepteur velit incididunt mollit dolore proident laborum aliqua
-          proident eiusmod culpa cillum id. Pariatur ea mollit occaecat deserunt
-          sit aliqua excepteur do do eiusmod nostrud cupidatat laborum.
-          Cupidatat laboris ex aute minim sint. Nulla esse aliquip Lorem elit
-          Lorem fugiat et laborum laborum.Aliqua aute nostrud cupidatat laborum
-          veniam dolore voluptate Lorem excepteur ut ut qui eu mollit. Elit
-          officia dolore labore est sunt tempor aute quis in fugiat adipisicing
-          Lorem sint nulla. Dolore reprehenderit mollit est dolor. Nisi mollit
-          non consectetur ullamco consequat amet magna commodo voluptate.
-        </p>
-        <p>
-          Consectetur eu excepteur ipsum aute. Dolore sint dolor occaecat quis
-          fugiat elit velit eu adipisicing. Ullamco adipisicing sit anim minim
-          dolor. Sint deserunt quis elit adipisicing non ex. Consectetur eiusmod
-          duis et culpa aliqua tempor ea magna consectetur.Consequat in ad sint
-          incididunt nostrud amet eiusmod non laboris occaecat et veniam dolor
-          enim. Mollit exercitation eiusmod ad dolore. Sit culpa consequat irure
-          laborum reprehenderit excepteur qui aute magna commodo ad eu. Sint qui
-          officia est sit nulla.
+          IEEE Edinburg Student Branch (IEEE ESB) is a dynamic, student-run
+          organization dedicated to fostering innovation, leadership, and
+          hands-on experience in engineering and technology. Based at UTRGV, our
+          branch operates under the global umbrella of the Institute of
+          Electrical and Electronics Engineers (IEEE), the world’s largest
+          technical professional organization.
         </p>
       </div>
     </div>
