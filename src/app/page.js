010a17c3f29e40be1c6ccc32a0b4a542affb1c09
Kory <koriellopez03@gmail.com>
2025-05-10 01:59:36 -0500

    updated about us

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,58 @@ export default function Home() {
         className={`object-none object-center h-48 w-full ${styles.event_image}`}
       />
       <p className="text-5xl font-bold text-primary_dark">About Us</p>
-      <div className="flex flex-col gap-3 w-3/4">
+      <div className="flex flex-col gap-5 w-3/4 items-center pb-20">
         <p>
           IEEE Edinburg Student Branch (IEEE ESB) is a dynamic, student-run
           organization dedicated to fostering innovation, leadership, and
           hands-on experience in engineering and technology. Based at UTRGV, our
           branch operates under the global umbrella of the Institute of
-          Electrical and Electronics Engineers (IEEE), the world’s largest
+          Electrical and Electronics Engineers (IEEE), the world's largest
           technical professional organization.
+        </p>
+        <h1 className="text-3xl font-bold">Who We Are</h1>
+        <p>
+          Open to students from all majors, IEEE ESB places a special emphasis
+          on those pursuing electrical and computer engineering. We aim to build
+          a collaborative environment where students can expand their technical
+          knowledge, grow their professional network, and make a real
+          impact—both on campus and in the wider community.
+        </p>
+        <h1 className="text-3xl font-bold">What We Do</h1>
+        <p>
+          We host a variety of events throughout the year to engage, educate,
+          and empower our members, including:
+        </p>
+        <ul className="list-disc">
+          <li>
+            Hands-on workshops in electronics, software development, robotics,
+            and more
+          </li>
+          <li>
+            Fundraisers and sponsorship initiatives to support projects and
+            outreach
+          </li>
+          <li>Student-led projects that encourage innovation and teamwork</li>
+          <li>
+            Mentorship and support programs to guide members through their
+            academic and career journeys
+          </li>
+          <li>
+            Community outreach, including visits to local grade schools to
+            inspire the next generation of engineers through fun, interactive
+            STEM activities
+          </li>
+        </ul>
+        <h1 className="text-3xl font-bold">Our Mission</h1>
+        <p>TBD</p>
+        <h1 className="text-3xl font-bold">Our Values</h1>
+        <p>TBD</p>
+        <h1 className="text-3xl font-bold">Why Join IEEE ESB?</h1>
+        <p>
+          Whether you're looking to enhance your technical skills, lead a team,
+          give back to your community, or simply find a group of like-minded
+          peers, IEEE ESB offers the tools and support to help you thrive. Join
+          us in shaping the future of engineering—starting today.
         </p>
       </div>
     </div>
