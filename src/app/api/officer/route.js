d994471290ff6952a2688e5bcdc1553bbc1e9fc1
Kory <koriellopez03@gmail.com>
2025-05-31 16:53:16 -0500

    begone logs

--- src/app/api/officer/route.js
+++ src/app/api/officer/route.js
@@ -7,10 +7,7 @@ export async function POST(req) {
   try {
     const data = await req.json();
     const users = await pb.collection("users").getFullList();
-    console.log(users);
     const userID = users.filter((user) => user.email == data.email)[0].id;
-    console.log(userID);
-    // console.log(user);
     await pb.collection("newOfficers").create({ user: userID });
     return NextResponse.json(data);
   } catch (e) {
@@ -18,16 +15,3 @@ export async function POST(req) {
     return NextResponse.error("Failed to update db");
   }
 }
-
-// {
-//   "avatar": "",
-//   "collectionId": "_pb_users_auth_",
-//   "collectionName": "users",
-//   "created": "2025-05-09 21:21:56.622Z",
-//   "email": "koriel.lopez01@utrgv.edu",
-//   "emailVisibility": false,
-//   "id": "55mlco319e9hy7o",
-//   "name": "Koriel Lopez",
-//   "updated": "2025-05-31 21:30:55.425Z",
-//   "verified": true
-// }
